d0c615068155d260b4e43faa4c4164c86d89aace
Sanhe Hu <6800411+MacHu-GWU@users.noreply.github.com>
2026-04-21 11:27:46 -0400

    update tests folder

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from afwf_github import api
+
+
+def test():
+    _ = api
+
+
 if __name__ == "__main__":
     from afwf_github.tests import run_cov_test
 
     run_cov_test(
         __file__,
-        "afwf_github",
-        is_folder=True,
+        "afwf_github.api",
         preview=False,
     )
