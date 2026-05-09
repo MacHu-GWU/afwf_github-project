2a83c180374f82f3117ffad25b2052ad4786218e
Sanhe Hu <6800411+MacHu-GWU@users.noreply.github.com>
2026-05-09 00:14:20 -0400

    update debug code

--- debug/debug_dataset.py
+++ debug/debug_dataset.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from afwf_github.dataset import (
-    repo_dataset,
-)
+from afwf_github.dataset import create_repo_dataset
+from afwf_github.paths import path_enum
+from afwf_github.config import Config
 from rich import print as rprint
 
 query = "afwf"
 
 if __name__ == "__main__":
-    res = repo_dataset.search(query=query, verbose=True, simple_response=True)
+    config = Config.load(path_enum.path_config_json)
+    repo_dataset = create_repo_dataset(config)
+    res = repo_dataset.search(query=query)
     rprint(res)
--- debug/debug_github.py
+++ debug/debug_github.py
@@ -1,26 +1,46 @@
 # -*- coding: utf-8 -*-
 
-from afwf_github.github import (
-    create_gh_client,
-    download_data,
-    get_user,
-    get_accounts,
-    get_repos,
-)
+from afwf_github.github import download_data
+from afwf_github.github import get_username
+from afwf_github.github import get_user
+from afwf_github.github import get_accounts
+from afwf_github.github import get_repos
+from afwf_github.paths import path_enum
+from afwf_github.config import Config
+from afwf_github.cache import make_cache
 from rich import print as rprint
 
 # page_limit = 3
 page_limit = 9999
 
-gh = create_gh_client()
-# download_data(gh, page_limit=page_limit, verbose=True)
+config = Config.load(path_enum.path_config_json)
+gh = config.gh
+user = get_username(gh)
+username = user["id"]
+user_dir = path_enum.dir_user(username)
+cache = make_cache(user_dir / ".cache")
 
-user = get_user()
+def _download_data():
+    download_data(
+        gh=gh,
+        cache=cache,
+        username=username,
+        expire=30 * 24 * 3600,
+        page_limit=page_limit,
+        verbose=True,
+    )
+
+_download_data()
+
+user = get_user(cache=cache, username=username)
 # rprint(user)
 
-accounts = get_accounts()
+accounts = get_accounts(cache=cache, username=username)
 # rprint(accounts[:10])
 
-repos = get_repos()
+repos = get_repos(cache=cache, username=username)
 # rprint(repos[:10])
 print(f"n repo = {len(repos)}")
+
+for repo in repos:
+    print(repo)
